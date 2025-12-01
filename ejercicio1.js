18a1da83ae675104456d532136b06e41a1cedd4e
josedavidlunamolano24-afk <josedavidlunamolano24@gmail.com>
2025-12-01 03:20:07 -0500

    h

---
+++ ejercicio1.js
@@ -0,0 +1,28 @@
+let filtro = document.getElementById("filtro");
+filtro.addEventListener("keyup", function() {
+
+    let texto = filtro.value.toLowerCase();
+    let tabla = document.getElementById("tabla");
+    let filas = tabla.getElementsByTagName("tr");
+
+    for (let i = 0; i < filas.length; i++) {
+
+        let celdas = filas[i].getElementsByTagName("td");
+        let mostrarFila = false;
+
+        for (let j = 0; j < celdas.length; j++) {
+
+            let contenidoCelda = celdas[j].innerText.toLowerCase();
+
+            if (contenidoCelda.includes(texto)) {
+                mostrarFila = true;
+                break;
+            }
+        }
+        if (mostrarFila) {
+            filas[i].style.display = "";
+        } else {
+            filas[i].style.display = "none";
+        }
+    }
+});
